854ec99c35a51bbf9f0022d814f6346a6b215a37
Clara <claraswanson671@gmail.com>
2022-08-19 17:46:36 +0200

    Minor aesthetic changes

--- script.js
+++ script.js
@@ -4,78 +4,112 @@
 const gameBoard = (() => { //module to control the gameboard
 
     const board = document.querySelectorAll('.box'); //array of boxes
-    let currentPlayer = 'anna';
     const resetButton = document.getElementById('reset');
+    const startButton = document.getElementById('start');
+    const announcement = document.getElementById('announcement');
+    resetButton.style.display = 'none';
+    let currentPlayer = 'clara';
 
     const placeXOrO = (index) => {
         let clickedBox = document.getElementById(index);
+        if (clickedBox.innerText === '' && startButton.style.display === 'none') {
             if (currentPlayer === 'clara') {
                 clickedBox.innerText = 'X';
+                currentPlayer = 'anna';
             } else if (currentPlayer === 'anna') {
                 clickedBox.innerText = 'O';
+                currentPlayer = 'clara';
             }
-    };
+        }
+        if (displayController.checkForWin(board)) {
+            announcement.innerText('X wins!')
+            clearBoard();
+        }
+    }
 
-    const clickEvent = () => {
+    const clickBoxEvent = () => {
         for (let box of board) {
             let i = box.getAttribute('id');
             box.addEventListener('click', () => {
                 placeXOrO(i);
-            });
-        };
+            })
+        }
     }
 
     const clearBoard = () => {
         resetButton.addEventListener('click', () => {
-            console.log('reset button clicked');
             for (let box of board) {
                 box.innerText = '';
-            };
+                startButton.style.display = '';
+                resetButton.style.display = 'none';
+            }
         })
-    };
+    }
 
     return {
-      clickEvent,
-      clearBoard
-    };
+        clickBoxEvent,
+        clearBoard
+    }
 
 })();
 
 gameBoard.clearBoard();
-gameBoard.clickEvent();
-
 
 const displayController = (() => { //module to control the flow of the game
 
-    // const clara = Player('X');
-    // const anna = Player('O');
+    const resetButton = document.getElementById('reset');
+    const startButton = document.getElementById('start');
 
-    const checkForRow = (board) => {
-        //code
+    const startGame = () => {
+        startButton.addEventListener('click', () => {
+            gameBoard.clickBoxEvent();
+            resetButton.style.display = '';
+            startButton.style.display = 'none';
+        })
     }
-    const checkForColumn = (board) => {
-        //code
-    }
-    const checkForDiagonal = (board) => {
-        //code
-    }
+
     const checkForWin = (board) => {
-        if (checkForRow || checkForColumn || checkForDiagonal) {
-            return true;
+
+        if (board[0] === 'X') {
+            if (board[1] === 'X' && board[2] === 'X') {
+                return true;
+            } else if (board[3] === 'X' && board[6] === 'X') {
+                return true;
+            } else if (board[4] === 'X' && board[8] === 'X') {
+                return true;
+            }
         }
-        return false;
+
+        if (board[8] === 'X') {
+            if (board[2] === 'X' && board[5] === 'X') {
+                return true;
+            } else if (board[6] === 'X' && board[7] === 'X') {
+                return true;
+            }
+        }
+
+        if (board[4] === 'X') {
+            if (board[1] === 'X' && board[7] === 'X') {
+                return true;
+            } else if (board[3] === 'X' && board[5] === 'X') {
+                return true;
+            }
+            else if (board[2] === 'X' && board[6] === 'X') {
+                return true;
+            }
+        }
+        
     }
 
     return {
+        startGame,
         checkForWin
-    };
+    }
 
 })();
 
+displayController.startGame();
 
-const Player = (name) => { //factory function for players
+/* const Player = (name) => { //factory function for players
 
-};
-
-// const clara = Player('clara');
-// const anna = Player('anna');
+}; */
