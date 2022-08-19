bd2a700e0a0ab01e861f2923bb50098dcaa2f2e9
Clara <claraswanson671@gmail.com>
2022-08-19 16:00:18 +0200

    Implement click and reset ability

--- script.js
+++ script.js
@@ -1,26 +1,81 @@
 "use strict"
 
-const GameBoard = (() => {
-    const board = []
-    return {
-      board
+
+const gameBoard = (() => { //module to control the gameboard
+
+    const board = document.querySelectorAll('.box'); //array of boxes
+    let currentPlayer = 'anna';
+    const resetButton = document.getElementById('reset');
+
+    const placeXOrO = (index) => {
+        let clickedBox = document.getElementById(index);
+            if (currentPlayer === 'clara') {
+                clickedBox.innerText = 'X';
+            } else if (currentPlayer === 'anna') {
+                clickedBox.innerText = 'O';
+            }
     };
-  })();
 
-  const DisplayController = (() => {
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-    return {
-      add,
-      sub,
-      mul,
-      div,
+    const clickEvent = () => {
+        for (let box of board) {
+            let i = box.getAttribute('id');
+            box.addEventListener('click', () => {
+                placeXOrO(i);
+            });
+        };
+    }
+
+    const clearBoard = () => {
+        resetButton.addEventListener('click', () => {
+            console.log('reset button clicked');
+            for (let box of board) {
+                box.innerText = '';
+            };
+        })
     };
-  })();
+
+    return {
+      clickEvent,
+      clearBoard
+    };
+
+})();
+
+gameBoard.clearBoard();
+gameBoard.clickEvent();
 
 
-const Player = () => {
+const displayController = (() => { //module to control the flow of the game
+
+    // const clara = Player('X');
+    // const anna = Player('O');
+
+    const checkForRow = (board) => {
+        //code
+    }
+    const checkForColumn = (board) => {
+        //code
+    }
+    const checkForDiagonal = (board) => {
+        //code
+    }
+    const checkForWin = (board) => {
+        if (checkForRow || checkForColumn || checkForDiagonal) {
+            return true;
+        }
+        return false;
+    }
+
+    return {
+        checkForWin
+    };
+
+})();
+
+
+const Player = (name) => { //factory function for players
 
 };
+
+// const clara = Player('clara');
+// const anna = Player('anna');
